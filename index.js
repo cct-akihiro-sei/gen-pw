916766842041a07c2245bf96d1dc5b7baac0462e
cct-akihiro-sei <akihiro.sei@cct-inc.co.jp>
2022-08-13 03:04:39 +0900

    modify: 間違えそうな文字を設定で除外

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ let passwords = generator.generateMultiple(100, {
     numbers: true,
     uppercase: false,
     excludeSimilarCharacters: true,
+    // その他の間違えそうな文字
+    exclude: [
+        '9q', // 発音が似ている
+        'mn', // 発音が似ている
+        'bd', // 発音が似ている
+    ].join(''),
 });
 
 console.log(passwords)
